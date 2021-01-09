959a26bb91ef3f02cf4ea947a31486da60b94895
DESKTOP-2G64TMN\kim <>
2021-01-09 22:04:24 +0900

    webpack build path 수정

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -7,12 +7,12 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "../dist"),
+    path: path.resolve(__dirname, "../build"),
     publicPath: '/'
   },
   mode: "development",
   devServer: {
-    contentBase: path.resolve(__dirname, "../dist"),
+    contentBase: path.resolve(__dirname, "../build"),
     index: "index.html",
     port: 9000
   },
